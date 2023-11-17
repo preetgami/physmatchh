d2740e1bfebaa580bed49169a64e077e37cfdbef
David Cao <sirdavidcao@gmail.com>
2023-11-16 21:19:10 -0700

    backend auth implementation

--- src/shared/Navigation/MainNavigation.js
+++ src/shared/Navigation/MainNavigation.js
@@ -9,40 +9,41 @@ import NavLinks from "./NavLinks";
 import SideDrawer from "./SideDrawer";
 import Backdrop from "../UIelemets/Backdrop";
 
-export default function MainNavigation(props) {
-    const [drawerIsOpen, setDrawerIsopen] = useState(false);
+export default function MainNavigation({ supabase, session }) {
+  const [drawerIsOpen, setDrawerIsopen] = useState(false);
 
-    const openDrawer = () => {
-        setDrawerIsopen(true);
-    };
+  const openDrawer = () => {
+    setDrawerIsopen(true);
+  };
 
-    const closeDrawer = () => {
-        setDrawerIsopen(false);
-    };
-    return (
-        <React.Fragment>
-            {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
+  const closeDrawer = () => {
+    setDrawerIsopen(false);
+  };
+  return (
+    <React.Fragment>
+      {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
 
-            <SideDrawer show={drawerIsOpen} onClick={closeDrawer}>
-                <nav className="main-navigation__drawer-nav">
-                    <NavLinks />
-                </nav>
-            </SideDrawer>
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawer}>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks supabase={supabase} session={session} />
+        </nav>
+      </SideDrawer>
 
-            <MainHeader>
-                <button className="main-navigation__menu-btn" onClick={openDrawer}>
-                    <span />
-                    <span />
-                    <span />
-                </button>
-                <h1 className="main-naviagtion__tile">
-                    <Link id="link_Styles" to="/">PhysMatch </Link>
-                  
-                </h1>
-                <nav className="main-navigation__header-nav">
-                    <NavLinks />
-                </nav>
-            </MainHeader>
-        </React.Fragment>
-    );
+      <MainHeader>
+        <button className="main-navigation__menu-btn" onClick={openDrawer}>
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-naviagtion__tile">
+          <Link id="link_Styles" to="/">
+            PhysMatch{" "}
+          </Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks supabase={supabase} session={session} />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
+  );
 }
--- src/shared/Navigation/NavLinks.js
+++ src/shared/Navigation/NavLinks.js
@@ -2,43 +2,55 @@ import React from "react";
 
 import { NavLink } from "react-router-dom";
 import "./NavLinks.css";
-import { Authcontex } from "../hooks/auth-context";
-import { useContext } from "react";
+import AuthFunction from "../../functions/AuthFunction";
+export default function NavLinks({ supabase, session }) {
+  const authFunction = AuthFunction(supabase);
 
-export default function NavLinks(props) {
-    const auth = useContext(Authcontex);
-    return (
-        <ul className="nav-links">
-            {!auth.islogedin &&
-            <li>
-                <NavLink to="/" exact>
-                    Book A Doc
-                </NavLink>
-            </li>}
-            {auth.islogedin &&
-                <li>
-                    <NavLink to="/clients">Clients</NavLink>
-                </li>
-            }
-            {auth.islogedin &&
-                <li>
+  const logoutUser = async () => {
+    const { error } = await supabase.auth.signOut();
+  };
 
-                    <NavLink to="/onboarding">Onboarding</NavLink>
-                </li>}
-            {auth.islogedin &&
-                <li>
-                    <NavLink to="/requests">Requests</NavLink>
-                </li>}
-            {!auth.islogedin &&
-                <li>
-                    <NavLink to="/auth">Doctor log-in</NavLink>
-                </li>
-            }
-                
+  return (
+    <ul className="nav-links">
+      {!session && (
+        <li>
+          <NavLink to="/" exact>
+            Book A Doc
+          </NavLink>
+        </li>
+      )}
+      {session && (
+        <li>
+          <NavLink to="/clients">Clients</NavLink>
+        </li>
+      )}
+      {session && (
+        <li>
+          <NavLink to="/onboarding">Onboarding</NavLink>
+        </li>
+      )}
+      {session && (
+        <li>
+          <NavLink to="/requests">Requests</NavLink>
+        </li>
+      )}
+      {!session && (
+        <li>
+          <NavLink to="/auth">Doctor log-in</NavLink>
+        </li>
+      )}
 
-            
-            {auth.islogedin &&
-                (<li> <button onClick={auth.logout}>Log out </button></li>)}
-        </ul>
-    );
+      {session && (
+        <li>
+          <button
+            onClick={() => {
+              logoutUser();
+            }}
+          >
+            Log out{" "}
+          </button>
+        </li>
+      )}
+    </ul>
+  );
 }
